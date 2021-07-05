ce5d3164a2f340d7f2e3a5cd670ae5d7d05cdd61
JKGlory <dudrhkd267@naver.com>
2021-07-05 17:57:58 +0900

    message

---
+++ 002.py
@@ -0,0 +1,35 @@
+
+
+def greet():
+    print("Hello, Bioinformatics")
+
+
+def greet1(name: str) -> None:
+    print(f"Hello, {name}")
+
+
+def greet2(num: int) -> int:
+    return num * 2
+
+greet()
+ret1= greet1("glroy")
+print(ret1)
+
+ret2 = greet2(3)
+print(ret2)
+
+
+
+
+greet()
+
+greet1("glory")
+
+
+
+ 
+ 
+
+
+
+
--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # bioinfo-lecture-2021-07
-# bioinfo-lecture-2021-07
-# bioinfo-lecture-2021-07
